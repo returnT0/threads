ff319c77aaedf8008bae8edc07487b35c2a2dbf5
returnT0 <vlvolkov25@gmai.com>
2023-09-18 01:10:03 +0200

    Completed Auth

---
+++ app/(auth)/onboarding/page.tsx
@@ -0,0 +1,8 @@
+async function Page() {
+    return (<div>
+            <h1 className="head-text">Onboarding</h1>
+            <p>Complete the onboarding process</p>
+        </div>)
+}
+
+export default Page
