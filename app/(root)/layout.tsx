4c3605540310be14a320dfbdc71266d78203a7ca
returnT0 <vlvolkov25@gmai.com>
2023-10-08 23:57:04 +0200

    updated layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,41 +2,44 @@ import React from "react";
 import type {Metadata} from "next";
 import {Inter} from "next/font/google";
 import {ClerkProvider} from "@clerk/nextjs";
+import {dark} from "@clerk/themes";
 
 import "../globals.css";
-import Topbar from "@/components/shared/Topbar";
-import RightSidebar from "@/components/shared/RightSidebar";
-import Bottombar from "@/components/shared/Bottombar";
 import LeftSidebar from "@/components/shared/LeftSidebar";
-
+import Bottombar from "@/components/shared/Bottombar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import Topbar from "@/components/shared/Topbar";
 
 const inter = Inter({subsets: ["latin"]});
 
 export const metadata: Metadata = {
-    title: "Threads",
+    title: "Threads", description: "A Next.js 13 Meta Threads application",
 };
 
 export default function RootLayout({
                                        children,
                                    }: {
-    children: React.ReactNode
+    children: React.ReactNode;
 }) {
-    return (<ClerkProvider>
-        <html lang="en">
-        <body className={`${inter.className} bg-dark-1`}>
-        <Topbar/>
+    return (
+        <ClerkProvider
+            appearance={{
+                baseTheme: dark,
+            }}>
+            <html lang='en'>
+            <body className={inter.className}>
+            <Topbar/>
 
-        <main className="flex flex-row ">
-            <LeftSidebar/>
-            <section className="main-container">
-                <div className="w-full max-w-4xl">
-                    {children}
-                </div>
-            </section>
-            <RightSidebar/>
-        </main>
-        <Bottombar/>
-        </body>
-        </html>
-    </ClerkProvider>)
+            <main className='flex flex-row'>
+                <LeftSidebar/>
+                <section className='main-container'>
+                    <div className='w-full max-w-4xl'>{children}</div>
+                </section>
+                <RightSidebar/>
+            </main>
+
+            <Bottombar/>
+            </body>
+            </html>
+        </ClerkProvider>);
 }
